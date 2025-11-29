54fe24a238b42e4da0f18141d78cbd1cd68fa9f7
ReMineral <zakaichi.main@gmail.com>
2025-11-29 20:51:54 +0700

    Create sw.js

---
+++ src/sw.js
@@ -0,0 +1,13 @@
+```js
+self.addEventListener("install", e => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  console.log("SW Activated");
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request));
+});
+```
